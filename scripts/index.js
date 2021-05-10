6b60b0da3c8c4cd72be4ed6f3578f6b117cc8e0c
Alex Albaran <albaranlex@icloud.com>
2021-05-10 20:32:28 +0200

    Updates

---
+++ scripts/index.js
@@ -0,0 +1,63 @@
+const timer = document.querySelector(".clock");
+const hour = document.querySelector(".hour-cont");
+const minute = document.querySelector(".min-cont");
+const second = document.querySelector(".sec-cont");
+
+var hr = 0;
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+    stoptime = false;
+    timerCycle();
+  }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle() {
+  if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+
+    if (sec < 10 || sec == 0) {
+      sec = "0" + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = "0" + min;
+    }
+    if (hr < 10 || hr == 0) {
+      hr = "0" + hr;
+    }
+
+    hour.innerHTML = hr;
+    minute.innerHTML = min;
+    second.innerHTML = sec;
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
+
+function resetTimer() {
+  hour.innerHTML = "00";
+  minute.innerHTML = "00";
+  second.innerHTML = "00";
+}
